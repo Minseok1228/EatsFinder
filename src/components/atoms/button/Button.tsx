dca20fd775d58f69525b7c475c8925200d70c350
origin1508 <origin1508@gmail.com>
2024-06-19 23:52:14 +0900

    ✨ Feat: Button 컴포넌트 수정 (#8)

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -9,11 +9,14 @@ const buttonVariants = cva(
       variant: {
         primary:
           'bg-primary-400 text-white hover:bg-primary-200 active:bg-primary-500 disabled:bg-primary-50 disabled:text-primary-200',
-        stroke: 'border border-primary-400 text-primary-400',
+        stroke:
+          'border border-primary-400 text-primary-400 hover:bg-primary-25 active:bg-primary-500 active:text-white',
+        grayStroke: 'border border-gray-300 text-gray-300',
         gray: 'bg-gray-100 text-gray-300',
+        error: 'bg-error text-white',
       },
       size: {
-        mini: 'w-[80px] h-[35px] rounded-[20px]',
+        mini: 'w-[80px] h-[35px] rounded-[20px] body-14',
         small: 'w-[160px]',
         medium: 'w-[280px]',
         large: 'w-[370px]',
@@ -30,10 +33,17 @@ interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {}
 
-export const Button = ({ className, variant, size, ...props }: ButtonProps) => {
+export const Button = ({
+  className,
+  variant,
+  size,
+  disabled,
+  ...props
+}: ButtonProps) => {
   return (
     <button
       className={twMerge(buttonVariants({ variant, size, className }))}
+      disabled={variant === 'gray' ? true : disabled}
       {...props}
     />
   );
