ecc164bbb7f56420468d9be54461907233b2b6cb
origin1508 <origin1508@gmail.com>
2024-06-20 23:23:30 +0900

    ✨ Feat: Checkbox variant 추가 (#6)

--- src/components/atoms/checkbox/Checkbox.tsx
+++ src/components/atoms/checkbox/Checkbox.tsx
@@ -1,16 +1,28 @@
-import { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes } from 'react';
 import {
   CheckBoxCheckedSVG,
   CheckBoxBlankSVG,
 } from '@/components/svg/CheckBoxSVG';
+import {
+  BookmarkDefaultSVG,
+  BookmarkFillSVG,
+} from '@/components/svg/BookmarkSVG';
+import { FavDefaultSVG, FavFillSVG } from '@/components/svg/FavSVG';
 import { twMerge } from 'tailwind-merge';
+
+const icon = {
+  default: [CheckBoxCheckedSVG(), CheckBoxBlankSVG()],
+  bookmark: [BookmarkFillSVG(), BookmarkDefaultSVG()],
+  fav: [FavFillSVG(), FavDefaultSVG()],
+};
 
 interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  variant?: keyof typeof icon;
 }
 
 export const Checkbox = ({
-  name,
+  variant = 'default',
   label,
   className,
   ...props
@@ -23,10 +35,11 @@ export const Checkbox = ({
           className,
         )}
       >
-        <input name={name} className='peer hidden' type='checkbox' {...props} />
+        <input className='peer hidden' type='checkbox' {...props} />
         <span className='mr-1 flex cursor-pointer items-center justify-center [&>svg:first-child]:hidden peer-checked:[&>svg:first-child]:block peer-checked:[&>svg:last-child]:hidden'>
-          {CheckBoxCheckedSVG()}
-          {CheckBoxBlankSVG()}
+          {icon[variant].map((it, idx) => (
+            <React.Fragment key={idx}>{it}</React.Fragment>
+          ))}
         </span>
         {label}
       </label>
