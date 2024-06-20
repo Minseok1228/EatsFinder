e301751c42287eee2a56c65e2de2bbb64024f302
origin1508 <origin1508@gmail.com>
2024-06-20 11:51:50 +0900

    💄 Style:	Checkbox 스타일 수정 (#6)

--- src/components/atoms/checkbox/Checkbox.tsx
+++ src/components/atoms/checkbox/Checkbox.tsx
@@ -1,8 +1,9 @@
+import { InputHTMLAttributes } from 'react';
 import {
   CheckBoxCheckedSVG,
   CheckBoxBlankSVG,
 } from '@/components/svg/CheckBoxSVG';
-import { InputHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -16,13 +17,19 @@ export const Checkbox = ({
 }: CheckBoxProps) => {
   return (
     <>
-      <label className={className}>
+      <label
+        className={twMerge(
+          'inline-flex items-center text-gray-700 body-14',
+          className,
+        )}
+      >
         <input name={name} className='peer hidden' type='checkbox' {...props} />
         <span className='mr-1 flex cursor-pointer items-center justify-center [&>svg:first-child]:hidden peer-checked:[&>svg:first-child]:block peer-checked:[&>svg:last-child]:hidden'>
           {CheckBoxCheckedSVG()}
           {CheckBoxBlankSVG()}
         </span>
         {label}
+        asdfasdfsadf
       </label>
     </>
   );
