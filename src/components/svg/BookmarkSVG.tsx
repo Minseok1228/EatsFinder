7e1306ddc749198059bfc52233a80751e395940b
origin1508 <origin1508@gmail.com>
2024-06-20 23:36:12 +0900

    ✨ Feat: BookmarkSVG 추가 (#6)

---
+++ src/components/svg/BookmarkSVG.tsx
@@ -0,0 +1,34 @@
+export const BookmarkDefaultSVG = () => {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M5 21V5C5 4.45 5.19583 3.97917 5.5875 3.5875C5.97917 3.19583 6.45 3 7 3H17C17.55 3 18.0208 3.19583 18.4125 3.5875C18.8042 3.97917 19 4.45 19 5V21L12 18L5 21ZM7 17.95L12 15.8L17 17.95V5H7V17.95Z'
+        fill='#A6A6A6'
+      />
+    </svg>
+  );
+};
+export const BookmarkFillSVG = () => {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M5 5V21L12 18L19 21V5C19 4.45 18.8042 3.97917 18.4125 3.5875C18.0208 3.19583 17.55 3 17 3H7C6.45 3 5.97917 3.19583 5.5875 3.5875C5.19583 3.97917 5 4.45 5 5Z'
+        fill='#FB5607'
+      />
+    </svg>
+  );
+};
