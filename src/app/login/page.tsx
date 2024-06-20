a5f912c31da76bb2f3bf0e9a8d6d6cd3e50520cb
jms <minsuk815@naver.com>
2024-06-20 13:15:19 +0900

    ✨ Feat: recentLoginMsg 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,9 @@
-import { Button } from '@/components/atoms';
-import { SocialLoginBtnGroup } from './_components/SocialLoginBtn';
-import { Spacer } from '@/components/atoms/button/Spacer';
+import { LoginPage } from './_components/LoginPage';
 
-export default function LoginPage() {
+export default function page() {
   return (
-    <div>
-      <Spacer y={32} />
-      <Button size={'large'}>로그인 하기</Button>
-      <Spacer y={32} />
-      <SocialLoginBtnGroup />
-      <Button size={'large'} variant={'stroke'}>
-        회원가입하기
-      </Button>
-    </div>
+    <>
+      <LoginPage />
+    </>
   );
 }
-11;
