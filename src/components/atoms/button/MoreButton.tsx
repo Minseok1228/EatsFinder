daead995cde7da41711ee8970e39b3f84eb5e9d0
origin1508 <origin1508@gmail.com>
2024-06-22 16:28:51 +0900

    💄 Style:	MoreButton 스타일 재작업

--- src/components/atoms/button/MoreButton.tsx
+++ src/components/atoms/button/MoreButton.tsx
@@ -1,27 +1,21 @@
 import { ButtonHTMLAttributes } from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
 import { twMerge } from 'tailwind-merge';
 
-const moreButtonVariants = cva(
-  'flex justify-center items-center w-[250px] h-[70px] rounded-[24px] subTitle-20',
-  {
-    variants: {
-      variant: {
-        primary:
-          'bg-primary-400 text-white hover:bg-primary-200 active:bg-primary-500 disabled:bg-white disabled:text-gray-200',
-        white: 'bg-white text-gray-700',
-      },
-    },
-    defaultVariants: { variant: 'primary' },
-  },
-);
+interface MoreButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  selected: boolean;
+}
 
-interface MoreButtonProps
-  extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof moreButtonVariants> {}
-
-export const MoreButton = ({ variant, ...props }: MoreButtonProps) => {
+export const MoreButton = ({ selected, ...props }: MoreButtonProps) => {
   return (
-    <button className={twMerge(moreButtonVariants({ variant }))} {...props} />
+    <button
+      className={twMerge(
+        'flex h-[70px] w-[250px] items-center justify-center rounded-[24px] text-gray-700 subTitle-20',
+        'hover:bg-primary-200 hover:text-white',
+        'active:bg-primary-500 active:text-white',
+        'disabled:bg-white disabled:text-gray-200',
+        selected && 'bg-primary-400 text-white',
+      )}
+      {...props}
+    />
   );
 };
