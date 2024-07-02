ccc79c6a7e79d2c97a23df37d6889e374ed8284a
jms <minsuk815@naver.com>
2024-07-02 19:00:05 +0900

    ♻️ Refactor: spacer 제거 후 margin 값 추가(#23)

--- src/app/(auth)/_components/AuthHeader.tsx
+++ src/app/(auth)/_components/AuthHeader.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export const AuthHeader = () => {
   return (
     <header>
-      <Link href='/' className='flex h-20 items-center justify-center'>
+      <Link href='/' className='flex h-20 items-center justify-center mb-[60px]'>
         <LogoTxtSVG />
       </Link>
     </header>
--- src/app/(auth)/login/_components/LoginPage.tsx
+++ src/app/(auth)/login/_components/LoginPage.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/atoms';
-import { Spacer } from '@/components/atoms/spacer/Spacer';
 import { SocialLoginBtnGroup } from './SocialLoginBtn';
 import Link from 'next/link';
 import { LoginForm } from './LoginForm';
@@ -7,12 +6,11 @@ import { LoginForm } from './LoginForm';
 export const LoginPage = () => {
   return (
     <>
-      <h2 className='text-gray-800 title-30'>로그인</h2>
-      <Spacer y={24} />
-      <LoginForm />
-      <Spacer y={32} />
-      <SocialLoginBtnGroup />
-      <Spacer y={120} />
+      <h2 className='mb-6 text-gray-800 title-30'>로그인</h2>
+      <div className='flex flex-col gap-8'>
+        <LoginForm />
+        <SocialLoginBtnGroup />
+      </div>
       <Link href='/signup'>
         <Button size={'large'} variant={'stroke'}>
           회원가입하기
