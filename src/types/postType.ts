b7032eb1e18face0e1aeae024acf6c295880c88a
origin1508 <origin1508@gmail.com>
2024-07-15 22:41:43 +0900

    ✨ Feat: PostContentType 정의 (#31)

---
+++ src/types/postType.ts
@@ -0,0 +1,26 @@
+export interface PostContentType {
+  id: number;
+  content: string;
+  thumbnailUrl: string;
+  imageUrl: string | null;
+  menuTag: string[];
+  keywordTag: string;
+  likeCount: number;
+  createdAt: string;
+  users: {
+    id: number;
+    nickname: string;
+    profileImage: string | null;
+  };
+  places: PlacesType;
+  starRatings: number;
+}
+
+export interface PlacesType {
+  id: number;
+  name: string;
+  address: string;
+  roadAddress: string;
+  x: number;
+  y: number;
+}
