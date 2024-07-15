57ea4967c263678194d6461c5cd0a1424f2fff47
origin1508 <origin1508@gmail.com>
2024-07-15 22:39:50 +0900

    ✨ Feat: images 도메인에 s3 추가 (#31)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      {
+        protocol: 'https',
+        hostname: 'eatsfinder-bucket.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
   },
 };
 
