f275b3f2bb76f5678d186ce5497f1ff7f4307979
jms <minsuk815@naver.com>
2024-07-20 01:51:51 +0900

    ✨ Feat: myFeedCard hover 추가(#35)

--- src/components/molecules/myFeedCard/MyFeedCard.tsx
+++ src/components/molecules/myFeedCard/MyFeedCard.tsx
@@ -1,9 +1,17 @@
+'use client';
 import { CardHoverMsg } from '@/components/atoms/cardHoverMsg';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export const MyFeedCard = () => {
+  const [isHover, setIsHover] = useState(false);
   return (
-    <div className='relative h-[408px] w-[250px]'>
+    <div
+      className='relative h-[408px] w-[250px]'
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <Image
         className='rounded-3xl object-cover'
         src='https://images.unsplash.com/photo-1719861032503-225fac307c59?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8'
@@ -12,7 +20,7 @@ export const MyFeedCard = () => {
         priority={true}
         sizes='(max-width: 250px) 100vw, 250px'
       />
-      <CardHoverMsg />
+      {isHover && <CardHoverMsg />}
     </div>
   );
 };
