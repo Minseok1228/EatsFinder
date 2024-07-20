792bf26a6f4d7f1e5e18b3a6b805f7a1c926cd96
origin1508 <origin1508@gmail.com>
2024-07-21 01:06:40 +0900

    ✨ Feat: AddSVG fill 속성을 상속으로 변경 (#36)

--- src/components/svg/AddSVG.tsx
+++ src/components/svg/AddSVG.tsx
@@ -4,10 +4,10 @@ export const AddDefaultSVG = () => {
       width='24'
       height='24'
       viewBox='0 0 24 24'
-      fill='none'
+      fill='inherit'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <path d='M11 13H5V11H11V5H13V11H19V13H13V19H11V13Z' fill='#A6A6A6' />
+      <path d='M11 13H5V11H11V5H13V11H19V13H13V19H11V13Z' fill='inherit' />
     </svg>
   );
 };
