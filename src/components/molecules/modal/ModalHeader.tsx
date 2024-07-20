1488bcd6d3cdd47c274e0a9df0b31e4255fe27e2
origin1508 <origin1508@gmail.com>
2024-07-20 19:47:26 +0900

    ✨ Feat: 시맨틱 태그 고려 (#37)

--- src/components/molecules/modal/ModalHeader.tsx
+++ src/components/molecules/modal/ModalHeader.tsx
@@ -3,15 +3,19 @@ import { CloseSVG } from '@/components/svg/CloseSVG';
 
 interface ModalHeaderProps {
   title: string;
-  subTitle: string;
+  description: string;
   onClose: () => void;
 }
 
-export const ModalHeader = ({ title, subTitle, onClose }: ModalHeaderProps) => {
+export const ModalHeader = ({
+  title,
+  description,
+  onClose,
+}: ModalHeaderProps) => {
   return (
     <header className='relative py-10'>
-      <h1 className='mb-1 text-center text-gray-800 title-34'>{title}</h1>
-      <h2 className='text-center text-gray-300 body-16'>{subTitle}</h2>
+      <h2 className='mb-1 text-center text-gray-800 title-34'>{title}</h2>
+      <p className='text-center text-gray-300 body-16'>{description}</p>
       <button
         className='absolute left-10 top-10 h-6 w-6'
         onClick={onClose}
