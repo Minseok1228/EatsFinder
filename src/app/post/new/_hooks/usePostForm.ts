53d626b5b6e0f52d24def3c1c25dd5db7736a0c5
origin1508 <origin1508@gmail.com>
2024-07-28 20:15:28 +0900

    ✨ Feat: usePostForm (#36)

---
+++ src/app/post/new/_hooks/usePostForm.ts
@@ -0,0 +1,15 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { postFormSchema } from '@/utils/zodSchema';
+
+const usePostForm = () => {
+  const method = useForm({
+    mode: 'onChange',
+    defaultValues: { mainImgIndex: 0, imgs: [], preview: [] },
+    resolver: zodResolver(postFormSchema),
+  });
+
+  return method;
+};
+
+export default usePostForm;
