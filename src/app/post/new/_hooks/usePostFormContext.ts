d79c6c589ab737353c10eaa946460ceb59c7c4ac
origin1508 <origin1508@gmail.com>
2024-07-28 20:15:40 +0900

    ✨ Feat: usePostFormContext (#36)

---
+++ src/app/post/new/_hooks/usePostFormContext.ts
@@ -0,0 +1,62 @@
+import { ChangeEvent } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { type PostFormValue } from '@/utils/zodSchema';
+
+const usePostFormContext = () => {
+  const { handleSubmit, watch, setValue, getValues } =
+    useFormContext<PostFormValue>();
+
+  const preview = watch('preview');
+  const mainImgIndex = watch('mainImgIndex');
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const imgs = getValues('imgs');
+    const preview = getValues('preview');
+    const MAX_CNT = 5;
+    const curCnt = imgs.length;
+    const addImgs = e.target.files;
+
+    if (addImgs === null) return;
+
+    if (MAX_CNT - curCnt < addImgs.length) {
+      alert('이미지는 최대 5장 까지 첨부 가능합니다.');
+    }
+
+    for (let i = 0; i < Math.min(addImgs.length, MAX_CNT - curCnt); i++) {
+      const img = addImgs[i];
+      imgs.push(img);
+      preview.push(URL.createObjectURL(img));
+    }
+
+    setValue('imgs', imgs);
+    setValue('preview', preview);
+  };
+
+  const handleMainImgChange = (index: number) => {
+    setValue('mainImgIndex', index);
+  };
+
+  const handleImageRemove = (index: number) => {
+    const imgs = getValues('imgs');
+    const preview = getValues('preview');
+    const mainImgIndex = getValues('mainImgIndex');
+    const newImgs = imgs.slice(0, index).concat(imgs.slice(index + 1));
+    const newPreview = preview.slice(0, index).concat(preview.slice(index + 1));
+    setValue('imgs', newImgs);
+    setValue('preview', newPreview);
+
+    if (index === mainImgIndex) setValue('mainImgIndex', 0);
+    if (index < mainImgIndex) setValue('mainImgIndex', mainImgIndex - 1);
+  };
+
+  return {
+    preview,
+    mainImgIndex,
+    handleSubmit,
+    handleImageChange,
+    handleImageRemove,
+    handleMainImgChange,
+  };
+};
+
+export default usePostFormContext;
