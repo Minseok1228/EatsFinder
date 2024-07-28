4d00ae38ffde51ef01e039483ec3f8a70b6ea0e4
origin1508 <origin1508@gmail.com>
2024-07-28 15:00:53 +0900

    💄 Style: width를 max-content 속성으로 수정 (#36)

--- src/components/atoms/chip/Chip.tsx
+++ src/components/atoms/chip/Chip.tsx
@@ -10,11 +10,11 @@ export const Chip = ({ emoji, text, selected = false }: ChipsProps) => {
   return (
     <span
       className={customTwMerge(
-        'rounded-[22px] border border-gray-100 px-[20px] py-[10px] text-gray-600 body-16',
+        'w-max rounded-[22px] border border-gray-100 px-[20px] py-[10px] text-gray-600 body-16',
         selected && 'border-gray-900 text-gray-900 subTitle-16',
       )}
     >
-      <span className='font-tossface mr-1'>{emoji}</span>
+      <span className='mr-1 font-tossface'>{emoji}</span>
       {text}
     </span>
   );
