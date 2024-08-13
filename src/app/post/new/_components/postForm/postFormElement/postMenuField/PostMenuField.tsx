955fc4067320a01f81d477c88483fac05b890481
origin1508 <origin1508@gmail.com>
2024-08-13 18:17:26 +0900

    ✨ Feat: 메뉴등록 input에 포커스될 시 추천 메뉴가 뜨도록 구현 (#36)

--- src/app/post/new/_components/postForm/postFormElement/postMenuField/PostMenuField.tsx
+++ src/app/post/new/_components/postForm/postFormElement/postMenuField/PostMenuField.tsx
@@ -1,16 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { MenuTag } from './MenuTag';
 import { PostFormLabel } from '../postFormLabel';
 import { getMenus } from '@/api/post';
 import usePostFormContext from '@/app/post/new/_hooks/usePostFormContext';
 
 export const PostMenuField = () => {
+  const [isShow, setIsShow] = useState(false);
   const { placeId, menus, handleMenuAdd, handleMenuRemove } =
     usePostFormContext();
   const [menu, setMenu] = useState('');
   const [recommendedMenus, setRecommendedMenus] = useState<
     { id: number; menu: string }[]
   >([]);
+  const recommendedMenusRef = useRef(null);
 
   useEffect(() => {
     const updateRecommendedMenus = async () => {
@@ -25,6 +27,10 @@ export const PostMenuField = () => {
       updateRecommendedMenus();
     }
   }, [placeId]);
+
+  useEffect(() => {
+    if (menus.length === 5) setIsShow(false);
+  }, [menus]);
 
   return (
     <div>
@@ -57,18 +63,32 @@ export const PostMenuField = () => {
               }
             }}
             onChange={(e) => setMenu(e.target.value)}
+            onFocus={() => setIsShow(true)}
+            onBlur={(e) => {
+              if (!(e.relatedTarget === recommendedMenusRef.current)) {
+                setIsShow(false);
+              } else {
+                e.target.focus();
+              }
+            }}
           />
         )}
-        <div className='absolute left-0 top-full z-50 mt-3 max-h-[25rem] w-full rounded-3xl bg-white px-5 py-6 shadow-[0_0_20px_rgba(0,0,0,0.1)]'>
-          <p className='text-gray-800 body-18'>이런 메뉴는 어떠세요?</p>
-          <div className='flex flex-wrap'>
-            {recommendedMenus.map((menu) => (
-              <MenuTag key={menu.id} onClick={() => handleMenuAdd(menu.menu)}>
-                {menu.menu}
-              </MenuTag>
-            ))}
+        {isShow && recommendedMenus.length > 0 && (
+          <div
+            ref={recommendedMenusRef}
+            className='absolute left-0 top-full z-50 mt-3 max-h-[25rem] w-full rounded-3xl bg-white px-5 py-6 shadow-[0_0_20px_rgba(0,0,0,0.1)]'
+            tabIndex={-1}
+          >
+            <p className='mb-6 text-gray-800 body-18'>이런 메뉴는 어떠세요?</p>
+            <div className='flex flex-wrap gap-3'>
+              {recommendedMenus.map((menu) => (
+                <MenuTag key={menu.id} onClick={() => handleMenuAdd(menu.menu)}>
+                  {menu.menu}
+                </MenuTag>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
