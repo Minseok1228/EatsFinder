2a8e51875c0f67d4e165a74a1e0b2c2934d272d4
origin1508 <origin1508@gmail.com>
2024-08-13 14:44:20 +0900

    🍱 Add: CircleCloseSVG 추가 (#36)

--- src/components/svg/CloseSVG.tsx
+++ src/components/svg/CloseSVG.tsx
@@ -11,3 +11,29 @@ export const CloseSVG = () => {
     </svg>
   );
 };
+
+export const CircleCloseSVG = () => {
+  return (
+    <svg
+      width='25'
+      height='24'
+      viewBox='0 0 25 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect x='0.5' width='24' height='24' rx='12' fill='#A6A6A6' />
+      <path
+        d='M7.54492 7.00781L17.8615 17.3244'
+        stroke='white'
+        stroke-width='2'
+        stroke-linecap='round'
+      />
+      <path
+        d='M17.8613 7.00781L7.54475 17.3244'
+        stroke='white'
+        stroke-width='2'
+        stroke-linecap='round'
+      />
+    </svg>
+  );
+};
