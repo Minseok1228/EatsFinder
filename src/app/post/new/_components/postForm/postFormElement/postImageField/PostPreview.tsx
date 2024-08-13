2037b8b9d88e5519e347b8469f3722b5dd395e8d
origin1508 <origin1508@gmail.com>
2024-08-13 14:40:33 +0900

    ✨ Feat: 대표 및 삭제 아이콘 호버시 나타나도록 구현 (#36)

--- src/app/post/new/_components/postForm/postFormElement/postImageField/PostPreview.tsx
+++ src/app/post/new/_components/postForm/postFormElement/postImageField/PostPreview.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import { customTwMerge } from '@/utils/customTwMerge';
-import { CloseSVG } from '@/components/svg/CloseSVG';
+import { CircleCloseSVG, CloseSVG } from '@/components/svg/CloseSVG';
 
 export const PostPreview = ({
   src,
@@ -15,22 +15,33 @@ export const PostPreview = ({
   onMainImgChange: () => void;
 }) => {
   return (
-    <div className='relative h-[250px] w-[250px] overflow-hidden rounded-3xl'>
+    <div
+      className={customTwMerge(
+        'relative z-50 h-[250px] w-[250px] cursor-pointer overflow-hidden rounded-3xl',
+      )}
+    >
       <Image src={src} fill alt='preview' />
       <div
         className={customTwMerge(
-          'absolute left-6 top-6 flex h-[26px] w-[50px] cursor-pointer items-center justify-center rounded-xl bg-gray-100 text-gray-300 subTitle-14',
-          isMainImg && 'bg-primary-400 text-white',
+          'group absolute h-full w-full rounded-3xl hover:backdrop-brightness-50',
+          isMainImg && 'shadow-[inset_0_0_0_4px_theme(colors.primary.400)]',
         )}
-        onClick={() => onMainImgChange()}
       >
-        대표
-      </div>
-      <div
-        className='absolute right-6 top-6 cursor-pointer'
-        onClick={onImageRemove}
-      >
-        <CloseSVG />
+        <div
+          className={customTwMerge(
+            'absolute left-6 top-6 hidden h-[26px] w-[50px] cursor-pointer items-center justify-center rounded-xl bg-gray-100 text-gray-300 subTitle-14 group-hover:flex',
+            isMainImg && 'flex bg-primary-400 text-white',
+          )}
+          onClick={() => onMainImgChange()}
+        >
+          대표
+        </div>
+        <div
+          className='absolute right-6 top-6 hidden cursor-pointer rounded-full group-hover:block'
+          onClick={onImageRemove}
+        >
+          <CircleCloseSVG />
+        </div>
       </div>
     </div>
   );
